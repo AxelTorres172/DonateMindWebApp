1f941ed912ddb1d1ad8b7a0d95166ebe9ad88d94
Axel Eduardo Ruiz Torres <156726255+AxelTorres172@users.noreply.github.com>
2025-08-14 20:03:38 -0600

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'suenos-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './css/styles.css',
+  './Js/firebase.js',
+  './Js/usuarioForm.js',
+  './manifest.json',
+  './assets/icon-192.png',
+  './assets/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
